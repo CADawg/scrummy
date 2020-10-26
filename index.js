7a5b9871e29dd0ff86878909f2266d30a3bd7e6d
Snaddyvitch-Dispenser <conor.howland@outlook.com>
2020-10-26 14:23:02 +0000

    Fix minor mistake

--- index.js
+++ index.js
@@ -163,7 +163,7 @@ client.on('message', async message => {
 
         if (command === 'undo') {
             await message.delete();
-            let users_mentioned = message.mentions.members.first();
+            let users_mentioned = message.mentions.members;
             if (message.mentions.members.size === 0) {
                 await send_short_lived_message(message.channel, "You didn't mention a user's submission to undo.");
                 return;
